ddb3a2aca26feda46a495be341090cca3bae11a7
DouglasEde <briancarter340@gmail.com>
2021-04-25 12:58:22 -0700

    setting up frame for websiteA

---
+++ src/Coaching.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Coaching() {
+  return (
+    <div className="Coaching">
+      <h1>Coaching</h1>
+    </div>
+  );
+}
+
+export default Coaching;
