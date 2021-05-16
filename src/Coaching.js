e7872ec220378c6500c536b2eadece9c7b677d2e
DouglasEde <briancarter340@gmail.com>
2021-05-16 07:35:43 -0700

    added carsouel component

--- src/Coaching.js
+++ src/Coaching.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Carousel from "./Components/Carousel"
 import "./App.css";
 
 function Coaching() {
   return (
     <div className="Coaching">
       <h1>Coaching</h1>
+      
     </div>
   );
 }
