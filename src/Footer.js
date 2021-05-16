d469cabadd86b24f43abe3bf7fc5bb9865152d1a
DouglasEde <briancarter340@gmail.com>
2021-05-16 13:21:17 -0700

    worked on footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <div className="row">
           <div className="col1">
             <h5>Contact</h5>
-            <ul>
+            <ul className='contactf'>
               <li>
                 {" "}
                 <a href="mailto: briancarter340@gmail.com">
@@ -18,7 +18,7 @@ function Footer() {
                     width="50"
                     alt="Email"
                   />
-                </a>
+                </a>{" "}
                 Gmail
               </li>
               <li>
@@ -30,53 +30,69 @@ function Footer() {
                     width="50"
                     alt="LinkedIn"
                   />
-                </a>
+                </a>{" "}
                 LinkedIn
               </li>
             </ul>
           </div>
           <div className="col2">
             <h5>Comunity</h5>
-            <ul>
-              <li>        <a href="https://discord.com/channels/818180464711041035/818180464711041038">
-          <img
-            src="iconfinder_91_Discord_logo_logos_4373196.png"
-            height="50"
-            width="50"
-            alt="DIscord"
-          />
-        </a> Discord</li>
-              <li>        <a href="https://github.com/Douglas4739">
-          <img
-            src="iconfinder_github-square-social-media_764894.png"
-            height="50"
-            width="50"
-            alt="GitHub"
-          />
-        </a> GitHub</li>
-              <li>        <a href="https://gitlab.com/Douglas4739">
-          <img
-            src="iconfinder_144_Gitlab_logo_logos_4373151.png"
-            height="50"
-            width="50"
-            alt="GitLab"
-          />
-        </a> GitLab</li>
+            <ul className='contactf'>
+              <li>
+                {" "}
+                <a href="https://discord.com/channels/818180464711041035/818180464711041038">
+                  <img
+                    src="iconfinder_91_Discord_logo_logos_4373196.png"
+                    height="50"
+                    width="50"
+                    alt="DIscord"
+                  />
+                </a>{" "}
+                Discord
+              </li>
+              <li>
+                {" "}
+                <a href="https://github.com/Douglas4739">
+                  <img
+                    src="iconfinder_github-square-social-media_764894.png"
+                    height="50"
+                    width="50"
+                    alt="GitHub"
+                  />
+                </a>{" "}
+                GitHub
+              </li>
+              <li>
+                {" "}
+                <a href="https://gitlab.com/Douglas4739">
+                  <img
+                    src="iconfinder_144_Gitlab_logo_logos_4373151.png"
+                    height="50"
+                    width="50"
+                    alt="GitLab"
+                  />
+                </a>{" "}
+                GitLab
+              </li>
             </ul>
           </div>
           <div className="col3">
             <h5>Social</h5>
-            <ul>
-              <li>        <a href="https://www.instagram.com/doug_dynasty4739/">
-          <img
-            src="iconfinder_1_Instagram_colored_svg_1_5296765 (1).png"
-            height="50"
-            width="50"
-            alt="Instagram"
-          />
-        </a> Instagram</li>
-              <li>Item2</li>
-              <li>Item 3</li>
+            <ul className='contactf'>
+              <li>
+                {" "}
+                <a href="https://www.instagram.com/doug_dynasty4739/">
+                  <img
+                    src="iconfinder_1_Instagram_colored_svg_1_5296765 (1).png"
+                    height="50"
+                    width="50"
+                    alt="Instagram"
+                  />
+                </a>{" "}
+                Instagram
+              </li>
+              <li></li>
+              <li></li>
             </ul>
           </div>
         </div>
