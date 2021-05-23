c96355508e7420e2845b70c2e2803ecc8f0034e0
DouglasEde <briancarter340@gmail.com>
2021-05-23 10:04:24 -0700

    removed styled links from about, mostly

--- src/About.js
+++ src/About.js
@@ -7,9 +7,6 @@ function About() {
       <div className="profileImage">
         <img src="mountainman.jpg" height="285" width="245" alt="Douglas Ede" />
         <h2>Investor/Software Engineer </h2>
-      </div>
-      <div>
-        <br />
         <section className="aboutText">
           Hi, welcome to my website. I am a veteran stock market investor that
           has branched out into the world of software engineering. I have a real
@@ -20,6 +17,10 @@ function About() {
           skills, please take a look at my
           <a href="https://www.linkedin.com/in/douglasede-webdev"> LinkedIn.</a>
         </section>
+      </div>
+      <div>
+        <br />
+
         <section className="resume">
           <h1>Resume</h1>
           <a href="https://docs.google.com/document/d/1mahwZMrSbL89ux1Ix986jgIE4pIl_imuUB1sXCt4i2I/edit?usp=sharing">
@@ -38,54 +39,6 @@ function About() {
             launched a small but successful business in the field.
           </p>
         </section>
-        <a href="mailto: briancarter340@gmail.com">
-          <img
-            src="iconfinder_google-gmail_7089163.png"
-            height="100"
-            width="100"
-            alt="Email"
-          />
-        </a>
-        <a href="https://www.instagram.com/doug_dynasty4739/">
-          <img
-            src="iconfinder_1_Instagram_colored_svg_1_5296765 (1).png"
-            height="100"
-            width="100"
-            alt="Instagram"
-          />
-        </a>
-        <a href="https://discord.com/channels/818180464711041035/818180464711041038">
-          <img
-            src="iconfinder_91_Discord_logo_logos_4373196.png"
-            height="100"
-            width="100"
-            alt="Discord"
-          />
-        </a>
-        <a href="https://github.com/Douglas4739">
-          <img
-            src="iconfinder_github-square-social-media_764894.png"
-            height="100"
-            width="100"
-            alt="GitHub"
-          />
-        </a>
-        <a href="https://gitlab.com/Douglas4739">
-          <img
-            src="iconfinder_144_Gitlab_logo_logos_4373151.png"
-            height="100"
-            width="100"
-            alt="GitLab"
-          />
-        </a>
-        <a href="https://www.linkedin.com/in/douglasede-webdev/">
-          <img
-            src="iconfinder_1_Linkedin_unofficial_colored_svg_5296501 (1).png"
-            height="100"
-            width="100"
-            alt="LinkedIn"
-          />
-        </a>
       </div>
     </div>
   );
