24670ff53b1cee8712ff52e63d5edfa585e7c466
DouglasEde <briancarter340@gmail.com>
2021-08-23 18:43:15 -0700

    added a tech I love section

--- src/Programing.js
+++ src/Programing.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 
 function Programing() {
   return (
-    <div className='programing'>
-      <h1>Programing</h1>
+    <div className="programing">
+      <h1>Technologies I Love:</h1>
+      <div className="technologies">
+        <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+        <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
+        <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
+        <img src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" />
+        <img src="https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" />
+        <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" />
+        <img src="https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white" />
+        <img src="https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white" />
+        <img src="https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white" />
+        <img src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" />
+        <img src="https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white" />
+      </div>
     </div>
   );
 }
