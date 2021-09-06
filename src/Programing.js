cadc19b5eb9a78e1684d3fb6f821150a9d642310
DouglasEde <briancarter340@gmail.com>
2021-09-05 18:53:28 -0700

    added Concretti app to Projects

--- src/Programing.js
+++ src/Programing.js
@@ -57,6 +57,36 @@ function Programing() {
       <div className="Projects">
         <h1>My Projects:</h1>
         <p>Click on a project image to take you to the live deployment.</p>
+        <h2>Production/Calculator App</h2>
+        <a href="https://democlient.vercel.app/AppSelect">
+          <img src="appsuite.jpg" alt="app suite" />
+        </a>
+        <p>
+          This project is a suite of applications for Concretti, an artisanal
+          concrete production company. The applications included in this suite
+          are the following: (1)Material Calculator and (2)Production/Inventory
+          Tracker. Material Calculator overview: This application is to be a
+          product material calculator that allows the user to (1) select the
+          color and (2)select the products to be manufactured in said color.
+          When the user has made all of their selections, (3) the application
+          will display the required materials for production of those products.
+          Production/Inventory Tracker overview: This application is to be a
+          production and inventory tracker. When a user engages with the
+          application, they must be able to: (1) Manually enter orders by color
+          and product. (2) Set priority on individual products. This process may
+          be initiated manually of automatically, depending on the following
+          production condition: (a) If a particular product that is already in
+          production is not completed in a predetermined amount of time, the
+          status of the product becomes a priority. (3) See that all orders
+          entered are given an In Production status. (4) See the status changed
+          to Produced when a product tagged with In Production status is
+          produced. (5) See the status changed to In Stock when a product tagged
+          with Produced is moved into the shipping and packing area and
+          packaging is completed.
+          <br />
+          **Link is a DEMO only. Please contact me to see the full application.
+        </p>
+        <br />
         <h2>Quiz app</h2>
         <a href="https://douglas4739.github.io/Quiz-App/">
           <img src="codingquiz.jpg" alt="quiz" />
@@ -68,10 +98,13 @@ function Programing() {
         <br />
         <h2>Bookmark app</h2>
         <a href="https://douglas4739.github.io/Bookmark-App/">
-          <img src="Bookmarks.jpg" alt="quiz" />
+          <img src="Bookmarks.jpg" alt="bookmarks" />
         </a>
-        <p>CRUD functional app that allows users to save bookmarks to their favorite websites with ratings descriptions and filter results by rating.
-           Built with jQuery, JavaScript, HTML and CSS.</p>
+        <p>
+          CRUD functional app that allows users to save bookmarks to their
+          favorite websites with ratings descriptions and filter results by
+          rating. Built with jQuery, JavaScript, HTML and CSS.
+        </p>
       </div>
     </div>
   );
