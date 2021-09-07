f5d9c61a09adcc423eaa7ca37ef3f910b31364b1
DouglasEde <briancarter340@gmail.com>
2021-09-06 19:52:57 -0700

    yep

--- src/Nav.js
+++ src/Nav.js
@@ -6,10 +6,10 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav>
-      <h3 className="nav-logo">
-        {" "}
-        <img src="signature.svg" height="300" width="300" alt="Signature" />
-      </h3>
+      <img src="signature.svg"
+      height="300" 
+      width="300" 
+      alt="Signature" />
       <ul className="nav-links">
         <Link to="/About">
           <li className="nav-list">Douglas Ede</li>
