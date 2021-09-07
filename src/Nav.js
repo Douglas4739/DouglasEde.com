0ae6665b970dffb5d5d4900535b7e62595ab7f9f
DouglasEde <briancarter340@gmail.com>
2021-09-06 20:20:52 -0700

    m

--- src/Nav.js
+++ src/Nav.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav>
-      <img src="public\signature.svg"
+      <img src="signature.svg"
       height="300" 
       width="300" 
       alt="Signature" />
